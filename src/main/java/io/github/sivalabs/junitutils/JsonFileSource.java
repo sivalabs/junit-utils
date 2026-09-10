5dfb3a777e4598a4a11c9556be6adb3c1cc12a92
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2026-09-10 15:28:40 +0200

    Clean up unused imports and annotations in `JsonFileSource`

--- src/main/java/io/github/sivalabs/junitutils/JsonFileSource.java
+++ src/main/java/io/github/sivalabs/junitutils/JsonFileSource.java
@@ -1,29 +1,19 @@
 package io.github.sivalabs.junitutils;
 
-import static org.apiguardian.api.API.Status.STABLE;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.apiguardian.api.API;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 /**
- * {@code @CsvFileSource} is a {@linkplain Repeatable repeatable}
- * {@link ArgumentsSource} which is used to load comma-separated value (CSV)
+ * {@code @JsonFileSource} is an {@link ArgumentsSource} which is used to load JSON
  * files from one or more classpath {@link #resources} or {@link #files}.
- *
- * <h2>Inheritance</h2>
  *
  * <p>This annotation is {@linkplain Inherited inherited} within class hierarchies.
  *
- * @since 5.0
- * @see CsvSource
  * @see org.junit.jupiter.params.provider.ArgumentsSource
  * @see org.junit.jupiter.params.ParameterizedClass
  * @see org.junit.jupiter.params.ParameterizedTest
@@ -32,7 +22,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@API(status = STABLE, since = "5.7")
 @ArgumentsSource(JsonFileArgumentsProvider.class)
 @SuppressWarnings("exports")
 public @interface JsonFileSource {
